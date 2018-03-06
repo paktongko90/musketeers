e2c3e3a8a61dc496ffa77d425f1b5a505e995b15
paktongko <paktongko90@gmail.com>
2018-03-06 18:48:37 +0800

    test

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,2 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 <h1>This Admin Dashboard</h1>
 <a href='<?php echo site_url('admin/login/logout') ?>'>Logout</a>
+</body>
+</html>
