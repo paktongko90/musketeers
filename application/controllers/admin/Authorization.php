d208f736a507d999339de1f62cc88af24f918a13
paktongko <paktongko90@gmail.com>
2018-03-23 19:31:32 +0800

    xjadi

--- application/controllers/admin/Authorization.php
+++ application/controllers/admin/Authorization.php
@@ -16,4 +16,20 @@ class Authorization extends Admin_Controller{
 			$this->loadTemplate($this->layout.'index');
 		}
 	}
+
+	public function setPermission(){
+		foreach($this->input->post('modules') as $module) {
+			$data = array(
+				'perm_id' => $module,
+				'user_id' => $this->input->post('user_id')
+				);
+
+			$exists = $this->db->get_where('perm_to_user',array('user_id' => $this->input->post('user_id'),'perm_id' => $module))->row();
+			if(count($exists) == 0 ) {
+
+				$this->db->insert('perm_to_user',$data);
+			}
+		}
+		redirect('admin/authorization');
+	}
 }
--- application/views/admin/authorization/index.php
+++ application/views/admin/authorization/index.php
@@ -1,26 +1,14 @@
 <h4>Assign User to section of permissions</h4>
-<?php echo form_open('/authorization/set'); ?>
-
-
-<div class="form-group">
-	<h4>Users</h4>
-	<select name="user_id" id="" class="form-control">
-		<?php foreach($this->db->get('users')->result() as $user) {?>
-			<option value="<?php echo $user->id; ?>"><?php echo $user->email ?></option>
+<?php echo form_open('admin/authorization/setpermission'); ?>
+	User<br>
+	<select name="user_id">
+		<?php foreach ($this->db->get('users')->result() as $user) { ?>
+		<option value="<?php echo $user->id; ?>"><?php echo $user->email ?></option>
 		<?php } ?>
 	</select>
-</div>
-
-
-<div class="form-group">
-	<h4>List of modules and sections</h4>
-		<?php foreach($this->db->get('perms')->result() as $screen) {?>
+	List of modules and sections<br>
+	<?php foreach($this->db->get('perms')->result() as $screen) {?>
 			<input type="checkbox" name="modules[]" value="<?php echo $screen->id; ?>" /> <span><?php echo strtolower($screen->definition) ?></span> <br />
 		<?php } ?>
-</div>
-
-<div class="form-group">
-	<input type="submit" class="btn btn-primary" value="set permissions" />
-</div>
-
+	<input type="submit" value="set permissions" />
 <?php echo form_close(); ?>
