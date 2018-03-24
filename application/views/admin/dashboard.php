18fa4fc48d38d4c2229c5afd17b7a09bc06bcf46
paktongko <paktongko90@gmail.com>
2018-03-24 17:08:46 +0800

    test

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,9 +1,9 @@
 <h1>This Admin Dashboard</h1>
 <?php 
  	$currentuser = $this->aauth->get_user()->id;
- 	if(! $this->aauth->is_allowed('CREATE_USER',$currentuser)){
+ 	if(! $this->aauth->is_allowed('NULL',$currentuser)){
 		echo 'tidak boleh view';
 	}else{
-		echo 'boleh view';
+		echo 'boleh view ye';
 	}
   ?>
