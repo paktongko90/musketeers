098d2bc44b60a3375e26a485c644d7cdae3cb5aa
paktongko <paktongko90@gmail.com>
2018-04-10 18:58:45 +0800

    add user id

--- application/views/admin/authorization/userdetails.php
+++ application/views/admin/authorization/userdetails.php
@@ -1,8 +1,9 @@
+	<?php echo $userid; ?>
 	<br>List of Permission of <?php echo $users->username; ?><br>
 	<?php 
-	echo form_open('admin/authorization/usergroup');
+	echo form_open('admin/authorization/deletepermuser');
 	foreach ($permissions as $permission) { ?>
 	<input type="checkbox" name="modules[]" value="<?php echo $permission->id; ?>" /> <span><?php echo strtolower($permission->definition) ?></span> <br />
-	<?php } 
-	echo form_close();
-	?>
+	<?php } ?>
+	<input type="submit" value="Delete Permission" />
+	<?php echo form_close() ?>
