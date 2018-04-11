6b6cc753a9f28ad85012f4ad351d70a44c1cb0c8
paktongko <paktongko90@gmail.com>
2018-04-11 17:04:11 +0800

    asdasd

--- application/views/admin/authorization/userdetails.php
+++ application/views/admin/authorization/userdetails.php
@@ -1,4 +1,3 @@
-	<?php echo $users->id; ?>
 	<br>List of Permission of <?php echo $users->username; ?><br>
 	<?php 
 	echo form_open('admin/authorization/deletepermuser');
@@ -6,5 +5,5 @@
 	<input type="text" name="user_id" value="<?php echo $users->id; ?>" hidden="">
 	<input type="checkbox" name="permissions[]" value="<?php echo $permission->id; ?>" /> <span><?php echo strtolower($permission->definition) ?></span> <br />
 	<?php } ?>
-	<input type="submit" value="Delete Permission" />
+	<input type="submit" value="Revoke Access" />
 	<?php echo form_close() ?>
