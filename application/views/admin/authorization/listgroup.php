11a29f7a164bbded552a800d7cc399ce3f80b987
paktongko90 <paktongko90@gmail.com>
2018-04-16 00:40:31 +0800

    asdas

--- application/views/admin/authorization/listgroup.php
+++ application/views/admin/authorization/listgroup.php
@@ -5,7 +5,7 @@
 	foreach ($group as $grp) {
  	?>
 	<tr>
-		<td><?php echo $grp->name; ?></td>
+		<td><a href='<?php echo base_url('admin/authorization/groupperm/'.$grp->id); ?>'><?php echo $grp->name; ?></a></td>
 		<td><?php echo $grp->definition; ?></td>
 		<td><a type="button" href="<?php echo site_url('admin/authorization/deletegroup/'.$grp->id); ?>"> <i class="fa fa-close"></i></a></td>
 	</tr>
