5eb2ca5b883549f6a8829fac25db57b82895dfd1
paktongko90 <paktongko90@gmail.com>
2018-04-21 12:36:46 +0800

    asdas

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -2,58 +2,18 @@
 <html>
 <head>
 	<title></title>
-
-	<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/css/sb-admin.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/css/plugins/morris.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url(); ?>assets/js/bootstrap.min.js" rel="stylesheet">
-<style>
-		ul {
-		    list-style-type: none;
-		    margin: 0;
-		    padding: 0;
-		}
-
-		li {
-		    display: inline;
-		}
-</style>
-   
-</head>
-<body>
-<div class="fixed-top">
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary rounded">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link">Left Link 1</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link">Left Link 2</a>
-    </li>
-  </ul>
-  <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link">Right Link 1</a>
-    </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">            Dropdown on Right</a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action with a lot of text inside of an item</a>
-      </div>
-    </li>
-  </ul>
-</nav>
-	<h1>Hello World</h1>
-
-
-<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/plugins/morris/raphael.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/plugins/morris/morris.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/js/plugins/morris/morris-data.js"></script>
+	<link href="<?php echo base_url(); ?>assets/css/navbaradmin.css" rel="stylesheet">
 </body>
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+</ul>
 </html>
