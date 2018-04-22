0957e39675e2a3e0b887b4a58417e65b84a91ad0
paktongko90 <paktongko90@gmail.com>
2018-04-22 10:23:14 +0800

    XCZXC

--- application/views/templates/index.php
+++ application/views/templates/index.php
@@ -3,17 +3,35 @@
 <head>
 	<title></title>
 	<link href="<?php echo base_url(); ?>assets/css/navbaradmin.css" rel="stylesheet">
+  <style>
+    .sidenav {
+    height: 100%;
+    width: 160px;
+    position: fixed;
+    z-index: 1;
+    top: 20;
+    left: 5;
+    background-color: #708090;
+    overflow-x: hidden;
+    padding-top: 0px;
+}
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #000000;
+    display: block;
+}
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+  </style>
+</head>
 </body>
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </li>
-</ul>
+<div class="sidenav">
+  <a href="#about">About</a>
+  <a href="#services">Services</a>
+  <a href="#clients">Clients</a>
+  <a href="#contact">Contact</a>
+</div>
 </html>
